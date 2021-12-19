fd3022aecc7c251b28d14211ade97460089a32fa
Sanakhatun Shaikh <Sanakhatun>
2021-12-20 00:13:27 +0530

    Implementation of Standalone Collections

--- springcore/src/main/java/com/springcore/standalone/collections/Test.java
+++ springcore/src/main/java/com/springcore/standalone/collections/Test.java
@@ -1,10 +1,18 @@
 package com.springcore.standalone.collections;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/springcore/standalone/collections/standalone_config.xml");
 
+		Person person = context.getBean("person", Person.class);
+		System.out.println(person.getFriendList());
+		System.out.println("Type of Standalone Collection:" + person.getFriendList().getClass().getName());
 	}
 
 }
