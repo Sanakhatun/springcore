619d1454980f77f83b8a59bcf4d190d0eabe9e2c
Sanakhatun Shaikh <Sanakhatun>
2021-12-26 15:13:13 +0530

    Standalone collections - map and properties implementation

--- springcore/src/main/java/com/springcore/standalone/collections/Person.java
+++ springcore/src/main/java/com/springcore/standalone/collections/Person.java
@@ -1,10 +1,24 @@
 package com.springcore.standalone.collections;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Properties;
 
 public class Person {
 
 	private List<String> friendList;
+
+	private HashMap<String, Integer> pincodeMap;
+
+	private Properties properties;
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
 
 	public List<String> getFriendList() {
 		return friendList;
@@ -14,9 +28,17 @@ public class Person {
 		this.friendList = friendList;
 	}
 
+	public HashMap<String, Integer> getPincodeMap() {
+		return pincodeMap;
+	}
+
+	public void setPincodeMap(HashMap<String, Integer> pincodeMap) {
+		this.pincodeMap = pincodeMap;
+	}
+
 	@Override
 	public String toString() {
-		return "Person [friendList=" + friendList + "]";
+		return "Person [friendList=" + friendList + ", pincodeMap=" + pincodeMap + ", properties=" + properties + "]";
 	}
 
 }
--- springcore/src/main/java/com/springcore/standalone/collections/Test.java
+++ springcore/src/main/java/com/springcore/standalone/collections/Test.java
@@ -13,6 +13,17 @@ public class Test {
 		Person person = context.getBean("person", Person.class);
 		System.out.println(person.getFriendList());
 		System.out.println("Type of Standalone Collection:" + person.getFriendList().getClass().getName());
+		
+		System.out.println("------------------------------------------------------------------------");
+		
+		System.out.println("HashMap" + person.getPincodeMap());
+		System.out.println("Type of Standalone Colection: " + person.getPincodeMap().getClass().getName());
+		
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("Database Properties" + person.getProperties());
+		System.out.println("Type of Standalone Colection: " + person.getProperties().getClass().getName());
+		
+		
 	}
 
 }
