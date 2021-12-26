1259b768c75bfe2be986c881d7bb88f4c6e59233
Sanakhatun Shaikh <Sanakhatun>
2021-12-26 22:42:46 +0530

    Stereotype Annotation implementation

--- springcore/src/main/java/com/springcore/stereotype/Student.java
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -1,5 +1,50 @@
 package com.springcore.stereotype;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//@Component("stud") //To explicitly provide name to reference variable of your choice
+@Component
 public class Student {
+
+	@Value("Sana Shaikh")
+	private String studentName;
+
+	@Value("Mumbai")
+	private String city;
+
+	@Value("#{addressList}")
+	private List<String> addressList;
+
+	public List<String> getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(List<String> addressList) {
+		this.addressList = addressList;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", city=" + city + ", addressList=" + addressList + "]";
+	}
 
 }
